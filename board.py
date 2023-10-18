8b0fe2e1315f83098f487666a36efcd95b0b4a95
Supak Rodklang <tong.7147@gmail.com>
2023-10-18 12:58:32 +0700

    swap

--- board.py
+++ board.py
@@ -4,13 +4,19 @@ from turtle import Turtle, Screen
 
 class Board:
     def __init__(self):
-        self.tiles = [[2, 1, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [15, 14, 13]]
-        # self.nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        self.tiles = [[2, 1, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [15, 14, 13, None]]
+        self.finish = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15,None]]
+        self.init_pos = (0,0)
+        self.void_pos = [3, 3]
+        self.t = Turtle()
+        self.t.hideturtle()
+        self.t.penup()
 
     def draw_tiles(self):
-        t = Turtle()
-        t.hideturtle()
-        t.penup()
+        self.t.clear()
+        # t = Turtle()
+        t=self.t
+        self.t.goto(self.init_pos)
         for row in range(len(self.tiles)):
             print(row)
             for col in range(len(self.tiles[row])):
@@ -26,3 +32,38 @@ class Board:
         #         print(ran)
         #         self.tiles[row].append(ran)
         #         self.nums.pop(ran)
+
+    def go_up(self):
+        down_to_void_pos = self.void_pos[0]+1, self.void_pos[1]
+        self.swap_with_void(down_to_void_pos)
+
+    def go_down(self):
+        # self.head.setheading(270)
+        up_to_void_pos = self.void_pos[0]-1, self.void_pos[1]
+        self.swap_with_void(up_to_void_pos)
+
+    def go_left(self):
+        right_to_void_pos = self.void_pos[0], self.void_pos[1]+1
+        self.swap_with_void(right_to_void_pos)
+
+
+    def go_right(self):
+        left_to_void_pos = self.void_pos[0], self.void_pos[1]-1
+        self.swap_with_void(left_to_void_pos)
+
+
+    def swap_with_void(self, up_to_void_pos):
+        row_void, col_void = self.void_pos
+        row_next, col_next = up_to_void_pos
+        if self.check_boundary(row_next, col_next):
+            self.tiles[row_void][col_void], self.tiles[row_next][col_next] = self.tiles[row_next][col_next], self.tiles[row_void][col_void]
+            self.void_pos[0] = row_next
+            self.void_pos[1] = col_next
+
+        self.draw_tiles()
+
+    def check_boundary(self, row_num, col_num):
+        if 0 <= row_num < 4:
+            if 0 <= col_num < 4:
+                return True
+        return False
--- main.py
+++ main.py
@@ -10,6 +10,13 @@ is_game_on = True
 
 board = Board()
 
+screen.onkeypress(fun=board.go_up, key="Up")
+screen.onkeypress(fun=board.go_down, key="Down")
+screen.onkeypress(fun=board.go_right, key="Right")
+screen.onkeypress(fun=board.go_left, key="Left")
+
+
+
 while is_game_on:
     screen.update()
     board.draw_tiles()
